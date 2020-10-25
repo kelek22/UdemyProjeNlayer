bb0661d097d7167528c12d92b8b89dc6b454bd66
Yunus Emre Kaya <yunusemrecode@gmail.com>
2020-10-25 12:04:33 +0300

    First change

---
+++ UdemyProjeNlayer.Core/Model/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UdemyProjeNlayer.Core.Model
+{
+   internal class Category
+    {
+
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDelete { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+
+
+    }
+}
